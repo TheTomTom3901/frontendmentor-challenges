86af7de2b92eb5538eae7f325ee51abd3d97895f
TomTom <thetomtom3901business@gmail.com>
2021-05-20 22:26:19 +0100

    extend tailwind theme with merged challenge themes

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,15 @@
+const mergedTheme = [].reduce((accTheme, currentTheme) => {
+  return Object.entries(currentTheme).reduce((acc, [key, value]) => {
+    acc[key] = { ...acc[key], ...value};
+    return acc;
+  }, accTheme)
+}, {})
+
 module.exports = {
   purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {}
+    extend: mergedTheme
   },
   variants: {
     extend: {}
