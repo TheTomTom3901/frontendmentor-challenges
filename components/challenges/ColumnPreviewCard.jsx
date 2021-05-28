a05352b6f5d7bc48ba93bceff0255ab5cb97038f
TomTom <thetomtom3901business@gmail.com>
2021-05-29 00:12:46 +0100

    add theme keys to reduce chance of overlapping colours

--- components/challenges/ColumnPreviewCard.jsx
+++ components/challenges/ColumnPreviewCard.jsx
@@ -3,9 +3,9 @@ import CustomHead from '../CustomHead'
 const Card = ({ bgColour, textColour, icon, title, description }) => (
   <div className={`${bgColour} p-12`}>
     <img src={icon} alt={`${title}-icon`} className="mb-10" />
-    <h1 className="text-very-light-gray font-big-shoulders-display uppercase text-4xl mb-8">{title}</h1>
-    <p className="text-transparent-white font-lexend-deca leading-6 mb-8 xl:mb-20" style={{ fontSize: 15 }}>{description}</p>
-    <button className={`rounded-full border-2 border-white bg-white py-3 px-8 ${textColour} font-lexend-deca text-sm hover:text-very-light-gray hover:bg-transparent`}>Learn More</button>
+    <h1 className="text-cpc-very-light-gray font-big-shoulders-display uppercase text-4xl mb-8">{title}</h1>
+    <p className="text-cpc-transparent-white font-lexend-deca leading-6 mb-8 xl:mb-20" style={{ fontSize: 15 }}>{description}</p>
+    <button className={`rounded-full border-2 border-white bg-white py-3 px-8 ${textColour} font-lexend-deca text-sm hover:text-cpc-very-light-gray hover:bg-transparent`}>Learn More</button>
   </div>
 )
 
@@ -15,25 +15,25 @@ const ColumnPreviewCard = ({ challenge: { id, title, link, fontUrl }}) => (
       <link rel="preconnect" href="https://fonts.gstatic.com"/>
       <style data-href={fontUrl}>{`@import url('${fontUrl}')`}</style>
     </CustomHead>
-    <div className="flex flex-col justify-center items-center px-6 py-16 bg-very-light-gray xl:h-screen xl:p-0">
+    <div className="flex flex-col justify-center items-center px-6 py-16 bg-cpc-very-light-gray xl:h-screen xl:p-0">
       <div className="rounded-lg overflow-hidden xl:flex xl:w-2/3">
         <Card
-          bgColour="bg-bright-orange"
-          textColour="text-bright-orange"
+          bgColour="bg-cpc-bright-orange"
+          textColour="text-cpc-bright-orange"
           icon={`/${id}/icon-sedans.svg`}
           title="Sedans"
           description="Choose a sedan for its affordability and excellent fuel economy. Ideal for cruising in the city or on your next road trip."
         />
         <Card
-          bgColour="bg-dark-cyan"
-          textColour="text-dark-cyan"
+          bgColour="bg-cpc-dark-cyan"
+          textColour="text-cpc-dark-cyan"
           icon={`/${id}/icon-suvs.svg`}
           title="SUVs"
           description="Take an SUV for its spacious interior, power, and versatility. Perfect for your next family vacation and off-road adventures."
         />
         <Card
-          bgColour="bg-very-dark-cyan"
-          textColour="text-very-dark-cyan"
+          bgColour="bg-cpc-very-dark-cyan"
+          textColour="text-cpc-very-dark-cyan"
           icon={`/${id}/icon-luxury.svg`}
           title="Luxury"
           description="Cruise in the best car brands without the bloated prices. Enjoy the enhanced comfort of a luxury rental and arrive in style."
--- components/challenges/StatsPreviewCard.jsx
+++ components/challenges/StatsPreviewCard.jsx
@@ -3,7 +3,7 @@ import CustomHead from '../CustomHead'
 const Stat = ({ value, label }) => (
   <div className="space-y-1 xl:space-y-2">
     <p className="font-bold text-white font-lexend-deca text-2xl tracking-wider">{value}</p>
-    <p className="text-slighter-transparent-white uppercase text-sm tracking-widest">{label}</p>
+    <p className="text-spc-slighter-transparent-white uppercase text-sm tracking-widest">{label}</p>
   </div>
 )
 
@@ -13,17 +13,21 @@ const StatsPreviewCard = ({ challenge: { id, title, link, fontUrl } }) => (
       <link rel="preconnect" href="https://fonts.gstatic.com" />
       <style data-href={fontUrl}>{`@import url(${fontUrl})`}</style>
     </CustomHead>
-    <div className="flex flex-col justify-center items-center bg-very-dark-blue px-6 py-20 xl:h-screen xl:p-0">
-      <div className="rounded-lg overflow-hidden font-inter bg-dark-desaturated-blue xl:flex xl:flex-row-reverse xl:w-9/12">
+    <div className="flex flex-col justify-center items-center bg-spc-very-dark-blue px-6 py-20 xl:h-screen xl:p-0">
+      <div className="rounded-lg overflow-hidden font-inter bg-spc-dark-desaturated-blue xl:flex xl:flex-row-reverse xl:w-9/12">
         <div className="relative">
           <img className="xl:hidden" src={`/${id}/mobile-header.jpg`} alt="mobile header" />
           <img className="hidden xl:block" src={`/${id}/desktop-header.jpg`} alt="desktop header" />
-          <div className="absolute bg-soft-violet mix-blend-multiply inset-0" />
+          <div className="absolute bg-spc-soft-violet mix-blend-multiply inset-0" />
         </div>
         <div className="flex flex-col flex-1 justify-between p-8 pt-10 text-center xl:p-16 xl:text-left">
           <div className="mb-10">
-            <h1 className="font-semibold text-white text-2xl mb-4 xl:mb-6 xl:text-4xl font-lexend-deca">Get <span className="text-soft-violet">insights</span> that help your business grow.</h1>
-            <p className="text-slight-transparent-white text-sm leading-6 xl:text-base">Discover the benefits of data analytics and make better decisions regarding revenue, customer experience and overall efficiency</p>
+            <h1 className="font-semibold text-white text-2xl mb-4 xl:mb-6 xl:text-4xl font-lexend-deca">
+              Get <span className="text-spc-soft-violet">insights</span> that help your business grow.
+            </h1>
+            <p className="text-spc-slight-transparent-white text-sm leading-6 xl:text-base">
+              Discover the benefits of data analytics and make better decisions regarding revenue, customer experience and overall efficiency
+            </p>
           </div>
           <div className="space-y-6 xl:flex xl:space-x-14 xl:space-y-0">
             <Stat value="10k+" label="companies" />
