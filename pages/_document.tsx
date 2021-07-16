304440bce88f62cee1841964103633d4b110e0c4
TomTom <thetomtom3901business@gmail.com>
2021-07-17 00:13:38 +0100

    update home page design, update font loading to match fonts.google usage

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -8,7 +8,9 @@ class MyDocument extends Document {
       <Html>
         <Head>
           <link rel="icon" type="image/png" sizes="32x32" href={addAssetPrefix('favicon.png')} />
-          <link rel="preconnect" href="https://fonts.gstatic.com" />
+          <link rel="preconnect" href="https://fonts.googleapis.com" />
+          <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="" />
+          <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400..700&display=swap" rel="stylesheet" />
         </Head>
         <body>
           <Main />
--- pages/index.tsx
+++ pages/index.tsx
@@ -6,15 +6,23 @@ type Props = {
 }
 
 const Home = ({ challenges }: Props) => (
-  <ul>
-    {challenges.map(({ id, title }) => (
-      <li key={id}>
-        <Link href={`/challenge/${id}`}>
-          <a>{title}</a>
+  <div className="max-w-3xl mx-auto p-8 font-inter md:pt-20">
+    <div className="mb-10">
+      <h1 className="text-3xl font-bold mb-4 md:text-5xl">Challenges</h1>
+      <h2 className="text-gray-600 leading-7">My collection of challenges from <a className="text-blue-500 font-medium underline hover:text-blue-700" href="https://www.frontendmentor.io/" target="_blank" rel="noopener noreferrer">Frontendmentor</a>, ranging in skill levels with a mixture of components, single page websites and multi page websites. Best viewed at <strong>375px</strong> or <strong>1440px</strong>, other sizes are likely to not match the exact designs</h2>
+    </div>
+    <div className="grid gap-4 sm:grid-cols-2">
+      {challenges.map(({ id, title }) => (
+        <Link key={id} href={`/challenge/${id}`}>
+          <a className="block hover:shadow">
+            <div className="border border-gray-200 rounded p-4 h-full sm:flex sm:items-center sm:justify-center sm:text-center">
+              <h3 className="text-lg font-medium text-gray-900">{title}</h3>
+            </div>
+          </a>
         </Link>
-      </li>
-    ))}
-  </ul>
+      ))}
+    </div>
+  </div>
 )
 
 export const getStaticProps = async () => ({ props: { challenges: getChallenges() } })
